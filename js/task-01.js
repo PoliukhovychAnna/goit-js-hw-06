b1f09f56943822fd52b8de28f10a6b10710e29f9
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-03-29 20:23:02 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const list = document.querySelector('#categories');
-console.dir(`Number of categories: ${list.children.length}`);
+console.log(`Number of categories: ${list.children.length}`);
 [...list.children].forEach(item => {
-    console.dir(`Category: ${item.firstElementChild.textContent}`);
-    console.dir(`Elements: ${item.lastElementChild.children.length}`);
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}`);
 })
 
